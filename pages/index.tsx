95754be45dd230b41ca4dd8bb9081c2d00c7b1c7
Serhii Kaistrenko <skaistrenko@gmail.com>
2019-09-08 20:47:46 -0400

    home page update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,70 @@
 import React from "react";
-import { withTranslation } from "../i18n";
-import PropTypes from "prop-types";
+// import { withTranslation } from "../i18n";
+import { WithTranslation } from "react-i18next";
+
 
 import Events from "../components/home/events";
 import PrayerRequest from "../components/home/prayerRequest";
 import PhotoCarousel from "../components/home/photoCarousel";
 import VisitUs from "../components/home/visitUs";
 
-const Homepage = () => {
-    return (
-        <div>
-            {/* Main picture */}
-            <div id="main-page-img" className="content-body">
-                <div className="card-body text-center ">
-                    <h1 className="px-3 text-shadow main-title">&#34;God is with you wherever you go&#34;</h1>
-                    <footer className="blockquote-footer">Joshua 1:9, WEB</footer>
+class Homepage extends React.Component <WithTranslation> {
+    getInitialProps = () => ({
+        namespacesRequired: ['common'],
+    });
+
+    render() {
+        return (
+            <div>
+                {/* Main picture */}
+                <div id="main-page-img" className="content-body">
+                    <div className="card-body text-center ">
+                        <h1 className="px-3 text-shadow main-title">&#34;God is with you wherever you go&#34;</h1>
+                        <footer className="blockquote-footer">Joshua 1:9, WEB</footer>
+                    </div>
                 </div>
-            </div>
-            <hr />
+                <hr />
 
-            <div className="row justify-content-center">
-                <div className="col-xxs-12 col-lg-10 m-x-auto">
-                    <Events />
-                    <PrayerRequest />
-                    <PhotoCarousel />
+                <div className="row justify-content-center">
+                    <div className="col-xxs-12 col-lg-10 m-x-auto">
+                        <Events />
+                        <PrayerRequest />
+                        <PhotoCarousel />
 
-                    {/* CONTACT US */}
-                    <section className="mt-5 pt-5">
-                        <div className="jumbotron jumbotron-fluid bg-light">
-                            <div className="container text-center">
-                                <blockquote className="blockquote">
-                                    <h2 className="mb-0 dispa">
-                                        &#34;How good and pleasant it is when God’s people live together in unity!&#34;
-                                    </h2>
-                                    <footer className="blockquote-footer">Psalm 133:1</footer>
-                                </blockquote>
-                                <p className="lead">
-                                    <a
-                                        id="contact-btn"
-                                        className="btn btn-outline-warning hvr-icon-forward"
-                                        href="contact-us/contact-us.html"
-                                        role="button"
-                                    >
-                                            Contact Us
-                                        <i className="fas fa-arrow-circle-right hvr-icon"/>
-                                    </a>
-                                </p>
+                        {/* CONTACT US */}
+                        <section className="mt-5 pt-5">
+                            <div className="jumbotron jumbotron-fluid bg-light">
+                                <div className="container text-center">
+                                    <blockquote className="blockquote">
+                                        <h2 className="mb-0 dispa">
+                                            &#34;How good and pleasant it is when God’s people live together in unity!&#34;
+                                        </h2>
+                                        <footer className="blockquote-footer">Psalm 133:1</footer>
+                                    </blockquote>
+                                    <p className="lead">
+                                        <a
+                                            id="contact-btn"
+                                            className="btn btn-outline-warning hvr-icon-forward"
+                                            href="contact-us/contact-us.html"
+                                            role="button"
+                                        >
+                                                Contact Us
+                                            <i className="fas fa-arrow-circle-right hvr-icon"/>
+                                        </a>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    </section>
+                        </section>
 
-                    <VisitUs />
+                        <VisitUs />
 
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+
 };
 
-Homepage.getInitialProps = async () => ({
-    namespacesRequired: ["common"],
-});
-
-Homepage.propTypes = {
-    t: PropTypes.func.isRequired,
-};
-
-export default withTranslation('common')(Homepage);
+export default Homepage;
