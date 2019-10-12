7bd1c16b366596c03122e3c72843d724860781bb
Serhii Kaistrenko <skaistrenko@gmail.com>
2019-10-12 16:58:16 -0400

    more lint

--- components/home/event.tsx
+++ components/home/event.tsx
@@ -21,16 +21,18 @@ class EventTile extends React.Component<Event, { image_url: string }> {
                 <div className='card-body'>
                     {
                         this.state.image_url &&
-                        (<img
-                            className='card card-img-top'
-                            src={`${this.state.image_url}w=320&q=80`}
-                            alt={this.props.image_description}
-                            srcSet={`
-                                ${this.state.image_url}w=320&q=80 1023w,
-                                ${this.state.image_url}w=260&q=80 1365w,
-                                ${this.state.image_url}w=320&q=80 5000w
-                            `}
-                        />)
+                        (
+                            <img
+                                className='card card-img-top'
+                                src={`${this.state.image_url}w=320&q=80`}
+                                alt={this.props.image_description}
+                                srcSet={`
+                                    ${this.state.image_url}w=320&q=80 1023w,
+                                    ${this.state.image_url}w=260&q=80 1365w,
+                                    ${this.state.image_url}w=320&q=80 5000w
+                                `}
+                            />
+                        )
                     }
                     <h5 className='card-title'>{this.props.title}</h5>
                     <h6 className='card-date'>{this.props.date}</h6>
