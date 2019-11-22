20781e8714a2e82b9badd31d60335b777b43bb75
Serhii Kaistrenko <skaistrenko@gmail.com>
2019-11-22 09:39:55 -0500

    moving to a new URL

--- lib/const.ts
+++ lib/const.ts
@@ -1,4 +1,3 @@
-export const pythonAPI = 'https://firstrussian-api.miki725.com/api/';
 export const defaultImages = {
     event: [
         'https://images.unsplash.com/photo-1512131852012-622823c149e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&',
@@ -31,4 +30,3 @@ export const getLocalizedMonths = (lan?: string) => {
 };
 
 export const isDevelopment = process.env.NODE_ENV !== 'production';
-export const nodeAPI = isDevelopment ? 'http://localhost:3000/api/' : 'https://firstrussian.miki725.com/api/';
