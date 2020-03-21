ee574f45654ff6a6ef71fa9dedb8bbaab0d16a08
Serhii Kaistrenko <skaistrenko@gmail.com>
2020-03-20 21:20:39 -0400

    package updates and config cleanup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const withCSS = require('@zeit/next-css');
-
 console.log("API_SITE_URL on build", process.env.API_SITE_URL);
 
-module.exports = withCSS({
+module.exports = {
     publicRuntimeConfig: {
         localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
             ? process.env.LOCALE_SUBPATHS
@@ -11,4 +9,4 @@ module.exports = withCSS({
     env: {
         MY_SITE_URL: process.env.API_SITE_URL,
     },
-});
+};
