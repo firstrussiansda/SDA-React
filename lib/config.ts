d1cfa1307162eec298bad515345e4e59d3700c39
Serhii Kaistrenko <skaistrenko@gmail.com>
2021-01-05 09:44:34 -0500

    Update map location, New York -> Brooklyn

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_PAGE_SIZE = 10;
+export const GOOGLE_MAPS_EMBED = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12117.297798897669!2d-73.9789855!3d40.6006647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4182e60b9560ea17!2sFirst%20Russian%20SDA%20Church!5e0!3m2!1sen!2sus!4v1609856964753!5m2!1sen!2sus';
